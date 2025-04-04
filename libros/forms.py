f7886c3160a908230b5b3395358d90fcc8b95bb7
Kevin Jovy <kev.castrillon.dev@gmail.com>
2025-04-04 17:53:50 -0500

    ✨feat: mejora el formulario
     Estilos Bootstrap y manejo de checkboxes

--- libros/forms.py
+++ libros/forms.py
@@ -3,17 +3,22 @@ from libros.models import Libro
 
 
 class LibroForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super(LibroForm, self).__init__(*args, **kwargs)
+        for field in self.fields.values():
+            field.widget.attrs.update({"class": "form-control"})
+
     class Meta:
         model = Libro
         fields = [
-            'titulo',
-            'autor',
-            'isbn',
-            'fecha_publicacion',
-            'descripcion',
-            'disponible',
+            "titulo",
+            "autor",
+            "isbn",
+            "fecha_publicacion",
+            "descripcion",
+            "disponible",
         ]
         widgets = {
-            'fecha_publicacion': forms.DateInput(attrs={'type': 'date'}),
-            'descripcion': forms.Textarea(attrs={'rows': 4}),
+            "fecha_publicacion": forms.DateInput(attrs={"type": "date"}),
+            "descripcion": forms.Textarea(attrs={"rows": 4}),
         }
